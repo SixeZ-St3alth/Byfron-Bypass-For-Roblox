fd5d2d38557929c24cce10c09591112283a1fd7c
SixeZ-St3alth <122944753+SixeZ-St3alth@users.noreply.github.com>
2023-05-23 21:32:43 -0400

    Add project files.

---
+++ IwaFweg345yer79e4rtg698ywe078Stg9y8orgh9y84wry879uoiweg9yo/Form1.cs
@@ -0,0 +1,10 @@
+namespace IwaFweg345yer79e4rtg698ywe078Stg9y8orgh9y84wry879uoiweg9yo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ IwaFweg345yer79e4rtg698ywe078Stg9y8orgh9y84wry879uoiweg9yo/Program.cs
@@ -0,0 +1,17 @@
+namespace IwaFweg345yer79e4rtg698ywe078Stg9y8orgh9y84wry879uoiweg9yo
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
